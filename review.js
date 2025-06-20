d570871f180625513831e72c895de1031f1c4130
seungwoo <zoozoo1302@gmail.com>
2025-06-20 18:29:51 +0900

    refactor: 로그화를 위해 console 부분을 looger로 변경 및 https로 변경

--- review.js
+++ review.js
@@ -1,16 +1,17 @@
 const db = require('./db');
+const logger = require('./log');
 
 // 내 리뷰 조회
 const getMyReview = async (req, res) => {
-    const { userId } = req.body;
+    const { userId } = req.params;
 
     try {
         const [rows] = await db.query('SELECT * FROM PLAN_REVIEW WHERE USER_ID = ?', [userId]);
 
         return res.status(200).json({success : true, message : "내 리뷰 조회 성공", reviews : rows});
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({error : "리뷰 조회 중 오류가 발생했습니다."});
+        logger.error(error);
+        return res.status(500).json({success : false, error : "리뷰 조회 중 오류가 발생했습니다."});
     }   
 }
 
@@ -26,16 +27,16 @@ const createReview = async (req, res) => {
         if(rating < 0 || rating > 5){
             await conn.rollback();
             conn.release();
-            console.error("리뷰 평점은 0~5 사이의 값이어야 합니다.");
-            return res.status(404).json({success : false, message : "비정상적인 접근입니다."});
+            logger.error("리뷰 평점은 0~5 사이의 값이어야 합니다.");
+            return res.status(404).json({success : false, error : "비정상적인 접근입니다."});
         }
 
         // 2. 리뷰 내용 검증
         if(review.length < 10 || review.length > 100){
             await conn.rollback();
             conn.release();
-            console.error("리뷰 내용은 10~100자 사이의 값이어야 합니다.");
-            return res.status(404).json({success : false, message : "비정상적인 접근입니다."});
+            logger.error("리뷰 내용은 10~100자 사이의 값이어야 합니다.");
+            return res.status(404).json({success : false, error : "비정상적인 접근입니다."});
         }
         
         await conn.query('INSERT INTO PLAN_REVIEW(USER_ID, PLAN_ID, STAR_RATING, REVIEW_CONTENT) VALUES(?, ?, ?, ?)', [userId, planId, rating, review]);
@@ -44,20 +45,21 @@ const createReview = async (req, res) => {
         if(rows.length === 0){
             await conn.rollback();
             conn.release();
-            console.error("존재하지 않는 플랜입니다.");
-            return res.status(404).json({success : false, message : "비정상적인 접근입니다."});
+            logger.error("존재하지 않는 플랜입니다.");
+            return res.status(404).json({success : false, error : "비정상적인 접근입니다."});
         }
 
         await conn.query('UPDATE PLAN_INFO SET RECEIVED_STAR_COUNT = ?, REVIEW_USER_COUNT = ? WHERE ID = ?', [rows[0].RECEIVED_STAR_COUNT + rating, rows[0].REVIEW_USER_COUNT + 1, planId]);
 
         await conn.commit();
         conn.release();
-        console.log(`${userId} 리뷰 작성 성공`);
+        logger.info(`${userId} 리뷰 작성 성공`);
         return res.status(200).json({success : true, message : "리뷰 작성 성공"});
     } catch (error) {
         await conn.rollback();
         conn.release();
-        console.error(error);
+        logger.error(error);
+        return res.status(500).json({success : false, error : "리뷰 작성 중 오류가 발생했습니다."});
     }
 }
 
@@ -74,8 +76,8 @@ const updateReview = async (req, res) => {
         if(reviews.length === 0){
             await conn.rollback();
             conn.release();
-            console.error("존재하지 않는 리뷰입니다.");
-            return res.status(404).json({success : false, message : "비정상적인 접근입니다."});
+            logger.error("존재하지 않는 리뷰입니다.");
+            return res.status(404).json({success : false, error : "비정상적인 접근입니다."});
         }
 
         await conn.query('UPDATE PLAN_REVIEW SET STAR_RATING = ?, REVIEW_CONTENT = ? WHERE REVIEW_ID = ?', [rating, review, reviewId]);
@@ -84,13 +86,13 @@ const updateReview = async (req, res) => {
 
         await conn.commit();
         conn.release();
-        console.log(`${reviewId} 리뷰 수정 성공`);
+        logger.info(`${reviewId} 리뷰 수정 성공`);
         return res.status(200).json({success : true, message : "리뷰 수정 성공"});
     } catch (error) {
         await conn.rollback();
         conn.release();
-        console.error(error);
-        return res.status(500).json({error : "리뷰 수정 중 오류가 발생했습니다."});
+        logger.error(error);
+        return res.status(500).json({success : false, error : "리뷰 수정 중 오류가 발생했습니다."});
     }
 }
 
@@ -108,8 +110,8 @@ const deleteReview = async (req, res) => {
         if(reviews.length === 0){
             await conn.rollback();
             conn.release();
-            console.error("존재하지 않는 리뷰입니다.");
-            return res.status(404).json({success : false, message : "비정상적인 접근입니다."});
+            logger.error("존재하지 않는 리뷰입니다.");
+            return res.status(404).json({success : false, error : "비정상적인 접근입니다."});
         }
 
         await conn.query('DELETE FROM PLAN_REVIEW WHERE REVIEW_ID = ?', [reviewId]);
@@ -119,21 +121,21 @@ const deleteReview = async (req, res) => {
         if(rows.length === 0){
             await conn.rollback();
             conn.release();
-            console.error("존재하지 않는 리뷰입니다.");
-            return res.status(404).json({success : false, message : "비정상적인 접근입니다."});
+            logger.error("존재하지 않는 리뷰입니다.");
+            return res.status(404).json({success : false, error : "비정상적인 접근입니다."});
         }
 
         await conn.query('UPDATE PLAN_INFO SET RECEIVED_STAR_COUNT = ?, REVIEW_USER_COUNT = ? WHERE ID = ?', [rows[0].RECEIVED_STAR_COUNT - reviews[0].STAR_RATING, rows[0].REVIEW_USER_COUNT - 1, reviews[0].PLAN_ID]);
 
         await conn.commit();
         conn.release();
-        console.log(`${reviewId} 리뷰 삭제 성공`);
+        logger.info(`${reviewId} 리뷰 삭제 성공`);
         return res.status(200).json({success : true, message : "리뷰 삭제 성공"});
     } catch (error) {
         await conn.rollback();
         conn.release();
-        console.error(error);
-        return res.status(500).json({error : "리뷰 삭제 중 오류가 발생했습니다."});
+        logger.error(error);
+        return res.status(500).json({success : false, error : "리뷰 삭제 중 오류가 발생했습니다."});
     }
 }
 
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 require('dotenv').config();
 const fs = require("fs");
+const https = require("https");
 const express = require("express");
 const expressWs = require('express-ws');
-const WebSocket = require('ws');
-const axios = require('axios');
 const cookieParser = require('cookie-parser');
 
 const app = express();
 expressWs(app);
-
 const cors = require("cors");
-const { signUp, phoneNumberDuplicate, emailAuth, checkAuth } = require('./member');
+
+const { signUp, phoneNumberDuplicate, emailAuth, checkAuth, getUserInfo, tokenCheck, logout, login, withDrawal, passwordChange, passwordReset, passwordCheck, phoneNumberCheck, authenticateToken } = require('./member');
 const { realtime, connections } = require('./chatbot');
+const { getMyReview, createReview, updateReview, deleteReview } = require('./review');
+const { getPlanList, getPlanDetail, filterPlans, changeUserPlan, recommendPlansByAge } = require('./plan');
+const logger = require('./log');
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(
     cors(
@@ -22,26 +27,96 @@ app.use(
     )
 );
 
-app.use(express.json());
+const options = {
+    key : fs.readFileSync(process.env.HTTPS_KEY),
+    cert : fs.readFileSync(process.env.HTTPS_CERT),
+    ca : fs.readFileSync(process.env.HTTPS_CA),
+};
 
-
-// 회원가입 관련 라우터
-app.post('/signUp', async (req, res) => await signUp(req, res));
-app.post('/duplicateCheck', async (req, res) => await phoneNumberDuplicate(req, res));
-app.post('/email', async (req, res) => await emailAuth(req, res));
-app.post('/checkAuth', async (req, res) => await checkAuth(req, res));
-
-// chat bot 관련 라우터
-app.ws('/realtime-chat', (clientWs, req) => {
-    console.log('🔗 WebSocket 연결 요청 받음:', req.url);
-    console.log('🔍 요청 헤더:', req.headers);
-    realtime(clientWs, req);
-  });
+// chat bot
+app.ws('/realtime-chat', (clientWs, req) => realtime(clientWs, req));
 
 app.get('/realtime-chat/connections', (req, res) => connections(req, res));
 
+/**
+ * 유저 관련
+ */
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
-});
+// 회원가입
+app.post('/signUp', async (req, res) => await signUp(req, res));
+
+// 휴대폰 중복확인
+app.post('/duplicateCheck', async (req, res) => await phoneNumberDuplicate(req, res));
+
+// 이메일 인증
+app.post('/email', async (req, res) => await emailAuth(req, res));
+
+// 이메일 인증 확인
+app.post('/checkAuth', async (req, res) => await checkAuth(req, res));
+
+// 비밀번호 변경
+app.post('/passwordChange', async (req, res) => await passwordChange(req, res));
+
+// 비밀번호 초기화
+app.post('/passwordReset', async (req, res) => await passwordReset(req, res));
+
+// 비밀번호 확인
+app.post('/passwordCheck', async (req, res) => await passwordCheck(req, res));
+
+// 휴대폰 번호 확인
+app.post('/phoneNumberCheck', async (req, res) => await phoneNumberCheck(req, res));
+
+// 회원탈퇴
+app.post('/withDrawal', async (req, res) => await withDrawal(req, res));
+
+// 로그인
+app.post('/login', async (req, res) => await login(req, res));
+
+// 로그아웃
+app.post('/logout', async (req, res) => await logout(req, res));
+
+// 토큰 체크(검증 및 갱신)
+app.post('/tokenCheck', authenticateToken, async (req, res) => await tokenCheck(req, res));
+
+// 유저 정보 조회
+app.post('/userInfo', async (req, res) => await getUserInfo(req, res));
+
+
+/**
+ * 리뷰 관련
+ */
+
+// 내 리뷰 조회
+app.get('/myReview/:userId', async (req, res) => await getMyReview(req, res));
+
+// 리뷰 작성
+app.post('/createReview', async (req, res) => await createReview(req, res));
+
+// 리뷰 수정
+app.post('/updateReview', async (req, res) => await updateReview(req, res));
+
+// 리뷰 삭제
+app.post('/deleteReview', async (req, res) => await deleteReview(req, res));
+
+
+/*
+ * 요금제 관련
+ * 
+ */
+
+// 전체 요금제 조회
+app.get('/planList', async (req, res) => await getPlanList(req, res));
+
+// 요금제 상세 정보 조회
+app.get('/planDetail/:planId', async (req, res) => await getPlanDetail(req, res));
+
+// 요금제 필터링 조회
+app.post('/filterPlans', async (req, res) => await filterPlans(req, res));
+
+// 요금제 변경
+app.post('/changeUserPlan', async (req, res) => await changeUserPlan(req, res));
+
+// 요금제 추천
+app.post('/recommendPlansByAge', async (req, res) => await recommendPlansByAge(req, res));
+
+https.createServer(options, app).listen(process.env.PORT, () => logger.info("서버가 연결되었습니다."));
